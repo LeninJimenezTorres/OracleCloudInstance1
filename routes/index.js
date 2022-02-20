fe74f680d514fc14f5f06b98f18542a951b5a4c5
LeninJimenezTorres <jlenin25@hotmail.com>
2022-02-20 09:53:48 -0500

    read json files directories with nodejs express

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
-
+var comparesDir='public/CompareFiles/';
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Basic NojeJs Application' });
+  res.render('layouts/index');
 });
 
+router.get('/compare/:t_id/:s1_id/:s2_id', function(req, res, next){
+  var datos={
+    "TRACKID": req.params.t_id,
+    "M_SESSIONID_1": req.params.s1_id,
+    "M_SESSIONID_2": req.params.s2_id,
+  };
+  var fileName=req.params.t_id+"-"+req.params.s1_id+"-"+req.params.s2_id+".json";
+  var output=JSON.stringify(datos);
+  var fs = require('fs');
+  fs.writeFile(comparesDir+fileName, output, function(err, result) {
+      if(err) console.log('error', err);
+      //else console.log(output);
+  });
+
+  fs = require('fs')
+  fs.readFile(comparesDir+fileName, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("READING: ");
+    console.log(data);
+  });
+
+  res.render('layouts/track',{pathOut: req.params.t_id+req.params.s1_id+req.params.s2_id});
+})
+
+router.get('/results/', function(req, res, next){
+
+
+});
 module.exports = router;
